27402f768d5c6aece5380f489ba492e0d47c9cb5
zhangyanfeng <zhangyanfeng@mafengwo.com>
2020-04-29 10:17:19 +0800

    初次提交，建立相关目录

---
+++ src/main/java/org/zyf/programming/ProgrammingApplication.java
@@ -0,0 +1,17 @@
+package org.zyf.programming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 描述：启动类操作
+ *
+ * @author yanfengzhang
+ * @date 2020-04-29 09:54
+ */
+@SpringBootApplication
+public class ProgrammingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammingApplication.class, args);
+    }
+}
---
+++ src/main/java/org/zyf/programming/arrayexercises/Test.java
@@ -0,0 +1,10 @@
+package org.zyf.programming.arrayexercises;
+
+/**
+ * 描述：测试类
+ *
+ * @author yanfengzhang
+ * @date 2020-04-29 10:11
+ */
+public class Test {
+}
